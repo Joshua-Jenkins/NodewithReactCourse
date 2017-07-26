a6f3db664ba6801865abb3eae4ace1d3194e1ba8
Joshua Jenkins <joshjenkins6@yahoo.com>
2017-07-26 16:31:59 -0500

    getting ready for predeployment settings for Heroku

--- index.js
+++ index.js
@@ -1,8 +1,12 @@
 const express = require('express');
 const app = express();
 
+// Route handler
 app.get('/', (req, res) => {
   res.send({ hi: 'there' });
 });
 
-app.listen(5000);
+//sever on localhost:5000
+// production use process.env.port || is for dev
+const PORT = process.env.PORT || 5000;
+app.listen(PORT);
