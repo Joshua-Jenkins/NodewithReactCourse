3a0082e4ed32f31280b971a4340100bbd477156c
Joshua Jenkins <joshjenkins6@yahoo.com>
2017-07-26 20:15:09 -0500

    added cookies session

--- services/passport.js
+++ services/passport.js
@@ -5,6 +5,16 @@ const keys = require('../config/keys');
 
 const User = mongoose.model('users');
 
+passport.serializeUser((user, done) => {
+  done(null, user.id);
+});
+
+passport.deserializeUser((id, done) => {
+  User.findById(id).then(user => {
+    done(null, user);
+  });
+});
+
 // console.developers.google.com
 passport.use(
   new GoogleStrategy(
@@ -14,7 +24,17 @@ passport.use(
       callbackURL: '/auth/google/callback'
     },
     (accessToken, refreshToken, profile, done) => {
-      new User({ googleId: profile.id }).save();
+      User.findOne({ googleId: profile.id }).then(existingUser => {
+        if (existingUser) {
+          // have a record already
+          done(null, existingUser);
+        } else {
+          // need to create a record
+          new User({ googleId: profile.id })
+            .save()
+            .then(user => done(null, user));
+        }
+      });
     }
   )
 );
